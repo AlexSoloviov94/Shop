bccf1ff054bf6979d3b5096fe4bf875f180588d1
AlexSoloviov94 <115881113+AlexSoloviov94@users.noreply.github.com>
2023-04-27 10:31:54 +0300

    Update HomeWork.tsx

--- src/pages/HomeWork/HomeWork.tsx
+++ src/pages/HomeWork/HomeWork.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { HomeProduct } from 'utils/productsHomeArray'
-import { useCallback } from 'react'
+
 
 import ContainerHomeWork from '../../components/ConteinerHomeWork/ConteinerHomeWork'
 
